c46756b8a64690f36d1ed04da7159da89738e33a
Fa'iq Hammam <hammamfaiq@protonmail.com>
2025-11-18 10:32:49 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rumah_sakit',
         'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'PASSWORD': 'kxkv87s8',
+        'HOST': 'dbprovider.ap-southeast-1.clawcloudrun.com',
+        'PORT': '40253',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
